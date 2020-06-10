4fd5e8a648c13856f4ac6ffa7a98a072601a3bc1
m4l4f4m4 <m4l4f4m4.8604@gmail.com>
2020-06-09 22:57:49 -0500

    solver leccion 3

---
+++ leccion_3/solver.py
@@ -0,0 +1,39 @@
+string_1 = "esto es una cadena de ejemplo"
+
+# print(string_1[1])
+
+# for letra in string_1:
+#     print(letra)
+#     pass
+
+lista_1 = [1, 2, 3, 'a', 'k', 10.56]
+
+# for item in lista_1:
+#     print(item)
+#     pass
+
+# for num in range(24, 0 ,-1):
+#     print(num)
+#     pass
+
+x = 5 
+y = 28
+z = 2
+
+# if (x == y):
+#     print('x es igual a y')
+#     pass
+# elif (x < y):
+#     print('x es menor que y')
+#     if (x > z):
+#         print('x es mayor z')
+        # pass
+    
+#     pass
+# else:
+#     print('x no es igual a y')
+#     pass 
+
+if x < y or x == y:
+    print('x menor que y pero tambien x no es igual y')
+    pass
